b6017eea2002010627f1c88f189a38569a2430ac
jaimecastignani <jaimecastignani@gmail.com>
2016-03-16 20:48:24 +0100

    Falta el refresco del ajax

--- public/javascripts/view.index.js
+++ public/javascripts/view.index.js
@@ -42,7 +42,31 @@ $(function () {
                 var msg = 'Status: ' + err.status + ': ' + err.responseText;
                 alert(msg);
             }
+
         });
         return false;
+        refresh();
     });
+
+    function refresh() {
+        $.ajax({
+            url: "/channel/add-message",
+            data: JSON.stringify({
+                name: $('#right-column h2').text(),
+                message: $('#new-message').val()
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $('<li>').appendTo('#messages').text(message);
+                $('#new-message').val('');
+                setInterval(refresh, 500);
+            },
+            error: function (err) {
+                var msg = 'Status: ' + err.status + ': ' + err.responseText;
+                alert(msg);
+            }
+        });
+    }
+
 });
